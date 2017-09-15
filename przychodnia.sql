d696e2cebba26e5b1de7960cca186e9ad6299a4f
Łukasz-Komputer\Łukasz <lukas.wojtkowiak@gmail.com>
2017-09-16 01:23:59 +0200

    PRZYCHODNIA - CLINIC - przygotowane triggery

--- przychodnia.sql
+++ przychodnia.sql
@@ -35,7 +35,8 @@ CREATE TABLE Visits (
 `IDdoctor` int NOT NULL,
 `IDpatient` int NOT NULL,
 `visitDate` date NOT NULL,
-`visithour` time not null,
+`visitHour` time not null,
+`visitStatus` varchar (15) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
 PRIMARY KEY (`IDvisit`))
 DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1;
 
@@ -60,12 +61,17 @@ DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci AUTO_INCREMENT=1;
 INSERT INTO Patients (patientName, patientSurname, dateofbirth, city, address, gender) values 
 ("Jan", "Nowak", "1967-09-25", "Wrocław", "Na Grobli 2/5", "M"),
 ("Janina", "Piła", "1923-11-02", "Wrocław", "Kościuszki 22b/15", "F"),
-("Jakub", "Wojdyn", "1990-02-01", "Oleśnica", "Na Grobli 12c/55", "M");
+("Jakub", "Wojdyn", "1990-02-01", "Oleśnica", "Na Grobli 12c/55", "M"),
+("Bogumił", "Wał", "1984-11-01", "Wrocław", "Kościuszki 2b/11", "M"),
+("Marianna", "Srocz", "1946-05-25", "Legnica", "Wrocławska 2", "F"),
+("Krystyna", "Gaz", "1977-07-07", "Wrocław", "Legnicka 5c/5", "F"),
+("Damian", "Dawidowicz", "1988-03-11", "Sobótka", "3", "M");
 
 INSERT INTO Doctors (doctorName, doctorSurname) values 
 ("Jerzy", "Karzeł"),
 ("Krystyna", "Danielczuk"),
-("Lech", "Kaczyński");
+("Lech", "Kaczyński"),
+("Maria", "Lewa-Tywa");
 
 INSERT INTO Workhours (IDdoctor, workstart, workend) values 
 (1, 06, 14),
@@ -73,10 +79,24 @@ INSERT INTO Workhours (IDdoctor, workstart, workend) values
 (3, 14, 22),
 (4, 22, 06);
 
+INSERT INTO  visits (IDdoctor, IDpatient,visitdate, visithour, visitstatus) values 
+(1,1,"2017-09-14", "07:30:00", "arranged"),
+(1,4,"2017-09-12", "09:30:00", "arranged"),
+(2,5,"2017-09-18", "12:30:00", "arranged"),
+(3,7,"2017-09-15", "14:00:00", "completed"),
+(4,6,"2017-09-14", "04:30:00", "completed");
+
+INSERT INTO prescriptions (IDvisit, presmedicines) values 
+(4, "Ibuprofen, cough syrup"),
+(5, "APAP, MAXIGRA");
 
 
-# TRIGGERS
 
+
+
+# TRIGGERS (wszystkie trigger rejestruja czas ustawienia wpisu w tabeli activities w formie data/godzina wpisu)
+
+# trigger dodaje informację do activities o tym, że pacjent dodał sie do bazy, jednocześnie ustawia mu wizytę, a że pacjent przyszedł nagle, tzn. że go boli a więc czas wizyty ustawia na teraz.
 delimiter |
 Create Trigger addPatient
 after insert on patients
@@ -87,15 +107,58 @@ set activityDescription= (select(concat("Pacjent : ", new.patientname," ", new.p
 insert into visits(iddoctor, idpatient, visitdate, visithour) values (1, (select idpatient from patients order by idpatient desc limit 1), curdate(),curtime());
 END
 |
-drop trigger addpatient;
+#trigger informuje o zmianie danych osobowych pacjenta o danym ID
+Create Trigger editionPatient
+after update on patients
+for each row
+begin
+insert into activities
+set activitydescription=(select(concat("Pacjent : ", new.patientname," ", new.patientsurname, " o numerze ", old.idpatient,  " zmienił swoje dane osobowe"))),  timeofactivity=now();
+end
 |
-select * from visits;
+# trigger miał dodawać wpis do activities odnośnie umówienia wizyty, miał być wypasiony ale jakoś nie moge dojść czemu nie działa, jeszcze go poprawię ...
+Create Trigger addVisit
+after insert on visits
+for each row
+begin
+insert into activities
+set activityDescription=((select(concat("Pacjent : ", p.patientname," ", p.patientsurname, " umówił wizytę na dzień ", new.visitdate, new.visithour))
+from patients as p, visits as v where p.idpatient=v.idpatient));
+#timeofactivity=now();
+end 
 |
-INSERT INTO Patients (patientName, patientSurname, dateofbirth, city, address, gender) values 
-("Jola", "Nowak", "1933-09-25", "Wrocław", "Na Sianie 2/12", "M");
+# trigger dodaje wpis do activities odnośnie wypisania recepty, a także ustawia na podstawie id wizyty status completed, bo rozumiem, że jeżeli jest recepta to wizyta juz sie odbyła
+Create Trigger addPrescription
+after insert on prescriptions
+for each row
+begin
+insert into activities
+set activityDescription=((select(concat("Wypisano receptę nr ", new.idprescription , " odnośnie wizyty nr ", new.idvisit)))), timeofactivity=now();
+update visits
+set visitstatus="completed" where idvisit=new.idprescription;
+end
+|
+/*
+TESTOWE KWERENDY
 
+select * from patients;
+|
+update patients set patientname="Dawid" where idpatient=7;
+|
+select * from activities;
 
-
+|
+drop trigger editionpatient;
+|
+INSERT INTO  visits (IDdoctor, IDpatient,visitdate, visithour, visitstatus) values 
+(2,3,"2017-09-15", "13:30:00", "arranged");
+|
+drop trigger addvisit;
+|
+INSERT INTO prescriptions (IDvisit, presmedicines) values 
+(3, "Ibuprofen, cough syrup");
+|
+select * from visits; */
 
 
 
